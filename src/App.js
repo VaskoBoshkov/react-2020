3309dad1e3b1b9f9c79610b7e8654a9d5ae8149f
unknown <shehata@msmo.io>
2020-01-04 12:54:53 +1100

    End of epi 07

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ import WeatherCard from "./components/WeatherCard/component";
 import "./App.css";
 
 function App() {
+  const data = async () => {
+    const apiRes = await fetch(
+      "http://api.openweathermap.org/data/2.5/weather?q=London,gb&units=metric&APPID=c80b69787ac9c776d1cf86f8a6c50ce7"
+    );
+    const resJSON = await apiRes.json();
+    return resJSON;
+  };
+  data().then(res => {
+    console.log("the feels like is " + res.main.feels_like);
+    console.log("the temp is " + res.main.temp);
+  });
+
   return (
     <div className="App">
       <WeatherCard temp={3} condition="Rain" city="Sydney" country="AU" />
